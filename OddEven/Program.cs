9d16a60fd5355837e44e55769f23cb25ed1e9734
Nikita Tolpekin <nikita.tolpekin@outlook.com>
2016-09-20 21:14:08 -0700

    Add files via upload

---
+++ OddEven/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class OddEven
+{
+    public static void Main(string[] args)
+    {
+        int number;
+
+        Console.Write("Enter integer: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+            Console.WriteLine("{0}", "This number is even");
+
+        else
+            Console.WriteLine("{0}", "This number is odd");
+    }
+}
